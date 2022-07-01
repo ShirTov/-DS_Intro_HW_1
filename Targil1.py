f2178d16c3c106ab7a1a41bc8a2a09378768b9f0
ShirTov <101014917+ShirTov@users.noreply.github.com>
2022-07-01 13:15:03 +0300

    Update Targil1.py

--- Targil1.py
+++ Targil1.py
@@ -1,4 +1,5 @@
 def my_func(x1,x2,x3):
+    #checking type of var
     check_x1 = isinstance(x1,float)
     check_x2 = isinstance(x2, float)
     check_x3 = isinstance(x3, float)
@@ -6,13 +7,10 @@ def my_func(x1,x2,x3):
     if (x1+x2+x3) == 0:
         return (print("Not a number – denominator equals zero"))
     if (check_x1 and check_x2 and check_x3):
-        return ((x1 + x2 + x3) * (x2 + x3) * x3) / (x1 + x2 + x3)
+        return float(((x1 + x2 + x3) * (x2 + x3) * x3) / (x1 + x2 + x3))
     else:
         print("Error: parameters should be float")
-
-ANS = my_func(,,)
-if (ANS != None):
-    print(ANS)
+    return None
 
 
 def convert(hours, minutes=0):
@@ -20,5 +18,10 @@ def convert(hours, minutes=0):
     if hours < 0 or minutes < 0:
         print("Input error!")
         exit(1)
+    if isinstance(hours, float):
+        h = str(hours)
+        hours = float(h.split('.')[0])
+        minutes = float(float(h.split('.')[1])/100*60)
+          
     return (hours * 3600 + 60 * minutes)
 
